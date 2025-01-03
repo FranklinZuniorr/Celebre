d0eb5573f0be4be9021dbcfaa952deed0594f9c4
Franklin <franklin.vieira@querodelivery.com>
2025-01-03 09:55:05 -0300

    initial commit

---
+++ src/main/java/celebre/celebre/CelebreApplication.java
@@ -0,0 +1,13 @@
+package celebre.celebre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelebreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/celebre/celebre/CelebreApplicationTests.java
@@ -0,0 +1,13 @@
+package celebre.celebre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelebreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
