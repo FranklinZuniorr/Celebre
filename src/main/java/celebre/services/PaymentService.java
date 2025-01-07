022f3bb53a25341bcb5e1816bd087adee2919020
Franklin <franklinbarbs763@gmail.com>
2025-01-06 21:58:35 -0300

    feat(base-product-checkout): should add new route to generate checkout url of base product

--- src/main/java/celebre/services/PaymentService.java
+++ src/main/java/celebre/services/PaymentService.java
@@ -1,9 +1,16 @@
 package celebre.services;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.stripe.Stripe;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+
+import celebre.entities.MessageResponseDto;
+import celebre.entities.PaymentCheckoutUrlDto;
 import celebre.helpers.Helpers;
 
 @Service
@@ -12,7 +19,39 @@ public class PaymentService {
     @Autowired
     Helpers helpers;
 
-    public ResponseEntity<String> payment() {
-        return helpers.<String>generateResponse(HttpStatus.OK, "Paymen success!");
+    @Value("${success.url}")
+    private String successUrl;
+
+    @Value("${cancel.url}")
+    private String cancelUrl;
+
+    @Value("${base.product.id}")
+    private String baseProductId;
+
+    @Value("${stripe.api.key}")
+    private String stripeApiKey;
+
+    public ResponseEntity<Object> checkoutProductBase() {
+        try {
+            Stripe.apiKey = stripeApiKey;
+
+            SessionCreateParams params =
+            SessionCreateParams.builder()
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl(successUrl)
+                .setCancelUrl(cancelUrl)
+                .addLineItem(
+                SessionCreateParams.LineItem.builder()
+                    .setQuantity(1L)
+                    .setPrice(baseProductId)
+                    .build())
+                .build();
+            Session session = Session.create(params);
+
+            return helpers.<Object>generateResponse(HttpStatus.OK, new PaymentCheckoutUrlDto(session.getUrl()));
+        } catch (Exception e) {
+            System.out.println(e);
+            return helpers.<Object>generateResponse(HttpStatus.INTERNAL_SERVER_ERROR, new MessageResponseDto("Não foi possível gerar o link de pagamento!"));
+        }
     }
 }
