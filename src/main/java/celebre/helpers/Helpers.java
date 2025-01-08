a0b4ee25290ed4f2c36a80c74be553d1f0bcf1a3
Franklin <franklinbarbs763@gmail.com>
2025-01-07 22:25:50 -0300

    att

--- src/main/java/celebre/helpers/Helpers.java
+++ src/main/java/celebre/helpers/Helpers.java
@@ -20,12 +20,9 @@ public class Helpers {
     @Value("${brevo.api.key}")
     private String brevoApiKey;
 
-    // Método para enviar e-mail via Brevo com Gson
     public boolean sendEmail(String to, String subject, String body) {
-        // URL de envio de e-mail da API do Brevo
         String apiUrl = "https://api.brevo.com/v3/smtp/email";
         
-        // Criar o corpo do e-mail em formato Map
         Map<String, Object> emailData = new HashMap<>();
         Map<String, String> sender = new HashMap<>();
         sender.put("email", "franklinbarbs763@gmail.com");
@@ -39,27 +36,21 @@ public class Helpers {
         emailData.put("subject", subject);
         emailData.put("htmlContent", body);
 
-        // Configurar cabeçalhos para a requisição
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("api-key", brevoApiKey);
 
-        // Usando Gson para converter o Map para JSON
         Gson gson = new Gson();
         String jsonBody = gson.toJson(emailData);
 
-        // Configurar a requisição HTTP
         HttpEntity<String> requestEntity = new HttpEntity<>(jsonBody, headers);
 
-        // Criar uma instância do RestTemplate para enviar a requisição
         RestTemplate restTemplate = new RestTemplate();
 
-        // Enviar a requisição e receber a resposta
         try {
             restTemplate.postForObject(apiUrl, requestEntity, String.class);
             return true;
         } catch (Exception e) {
-            System.out.println(e);
             return false;
         }
     }
