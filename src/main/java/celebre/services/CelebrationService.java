ddece079de3a0f76be1c6f217d81c831de08c934
Franklin <franklinbarbs763@gmail.com>
2025-01-08 22:23:29 -0300

    feat(celebration-service): should add new return structure

--- src/main/java/celebre/services/CelebrationService.java
+++ src/main/java/celebre/services/CelebrationService.java
@@ -1,21 +1,11 @@
 package celebre.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.google.gson.Gson;
-import com.stripe.Stripe;
-import com.stripe.model.checkout.Session;
-import com.stripe.param.checkout.SessionCreateParams;
-
 import celebre.entities.MessageResponseDto;
-import celebre.entities.PaymentConfirmationProductBaseDto;
-import celebre.enums.EnumEventType;
-import celebre.entities.MetadataPaymentProductBaseDto;
-import celebre.entities.PaymentCheckoutUrlDto;
 import celebre.helpers.Helpers;
 import celebre.models.Celebration;
 import celebre.repositories.CelebrationRepository;
@@ -35,8 +25,10 @@ public class CelebrationService {
     public ResponseEntity<Object> getCelebration(String id) {
         try {
             Celebration celebration = celebrationRepository.getCelebration(id);
+            Map<String, Celebration> payload = new HashMap<>();
+            payload.put("celebration", celebration);
 
-            return helpers.<Object>generateResponse(HttpStatus.OK, celebration);
+            return helpers.<Object>generateResponse(HttpStatus.OK, payload);
         } catch (Exception e) {
             System.out.println(e);
             return helpers.<Object>generateResponse(HttpStatus.BAD_REQUEST, new MessageResponseDto("A celebração não foi encontrada!"));
