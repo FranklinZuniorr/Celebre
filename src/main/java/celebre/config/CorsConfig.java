7464c4913218fa5bc640ba8327b41354f30eb983
Franklin <franklinbarbs763@gmail.com>
2025-02-05 23:43:01 -0300

    feat(cors)

--- src/main/java/celebre/config/CorsConfig.java
+++ src/main/java/celebre/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://seu-front-end.com")
+                        .allowedOrigins("http://localhost:4200", "https://celebre.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
